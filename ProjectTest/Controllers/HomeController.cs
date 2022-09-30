d76207f96fa7ffbf6b3af77dd52e12d342f3d333
Monus <Monus@LAPTOP-IGF5BLO4>
2022-09-30 15:40:27 +0530

    Add project files.

---
+++ ProjectTest/Controllers/HomeController.cs
@@ -0,0 +1,88 @@
+﻿using ProjectTest.Database;
+using ProjectTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        AniketTestEntities1 db = new AniketTestEntities1();
+        
+        public ActionResult CreateOrder() {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateOrder(OrderDetails model)
+        {
+            Order_datails orderdetails = new Order_datails
+            {
+                Id = model.Id,
+                customer = model.customer,
+                product = model.product,
+                remarks = model.remarks,
+            };
+
+            if (model.Id == 0)
+            {
+
+                db.Order_datails.Add(orderdetails);
+                db.SaveChanges();
+            }
+            else
+            {
+                db.Entry(orderdetails).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+            }
+            return RedirectToAction("ShowData");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var res = db.Order_datails.Select(x => new OrderDetails()
+            {
+                Id = x.Id,
+                customer = x.customer,
+                product = x.customer,
+                remarks = x.remarks,
+               
+            }).SingleOrDefault(x => x.Id == id);
+            return View("CreateOrder", res);
+        }
+
+        public ActionResult ShowData()
+        {
+
+            // List<EmployeeModel> emp = new List<EmployeeModel>();
+
+            var result = db.Order_datails
+                .Select(x => new OrderDetails()
+                {
+                    Id = x.Id,
+                    customer = x.customer,
+                    product = x.customer,
+                    remarks = x.remarks,
+                }).ToList();
+
+            return View(result);
+
+        }
+
+
+        public ActionResult Delete(int id)
+        {
+
+            var res = db.Order_datails.FirstOrDefault(x => x.Id == id);
+
+            db.Order_datails.Remove(res);
+            db.SaveChanges();
+            return RedirectToAction("ShowData");
+
+
+        }
+    }
+}
---
+++ ProjectTest/Models/OrderDetails.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectTest.Models
+{
+    public class OrderDetails
+    {
+        public int Id { get; set; }
+        public string customer { get; set; }
+        public string product { get; set; }
+        public string remarks { get; set; }
+    }
+}
